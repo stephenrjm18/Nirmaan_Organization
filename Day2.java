7fa856bf0252f4534593ab4d1509428ff6254830
stephenrjm18 <stephenrjm18@gmail.com>
2024-11-21 12:41:01 +0530

    Add files via upload
    Day2

---
+++ Day2.java
@@ -0,0 +1,28 @@
+public class Day2{
+public static void main(String[] args){
+
+String name1="Stephen";
+String name2="Samantha";
+String adjective1 ="happy";
+String adjective2 = "wonderful";
+String adjective3 ="excited";
+String noun1 ="birds";
+String noun2 = "chocolate";
+String noun3="guitar";
+String noun4="children";
+String noun5="ice cream";
+String noun6= "robots";
+String verb1="jump";
+int number=2050;
+String place1="Hawaii";
+
+
+
+String story = "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a " + adjective2 + " day!' Outside, a bunch of " + noun1+ "s were protestingto keep " + noun2 + " in stores. They began to " + verb1 + " to the rhythm of the " + noun3 + ", which made all the " + noun4 + "s very " + adjective3 + " .Concerned, " + name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped " + name1 + " in a puddle of frozen "+noun5 + ". " + name1 + " woke up in the year " + number + ", in a world where " + noun6 + "s ruled the world.";
+
+
+System.out.println(story);
+}}
+
+
+
