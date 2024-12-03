b55cbedfa937407696e1d2e30e9fb85af800ba39
stephenrjm18 <stephenrjm18@gmail.com>
2024-12-03 12:58:25 +0530

    Add files via upload
    Day8

---
+++ day8/Streqls.java
@@ -0,0 +1,19 @@
+package day8;
+
+public class Streqls
+{
+	public static void main(String[] agrs) 
+	{
+		String Hero = "Surya";
+		String str = new  String("Surya");
+		
+		if(Hero.equals(str)) 
+		{
+			System.out.println("Surya weds Jo");
+		}
+		else
+		{
+			System.out.println("Surya weds meghna");
+		}
+	}
+}
