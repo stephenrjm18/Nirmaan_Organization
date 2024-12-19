617ae397444cf257719df69e8b5cbc3b4edf8ecd
stephenrjm18 <stephenrjm18@gmail.com>
2024-12-19 10:12:10 +0530

    Update MainBank.java

--- assessment2/MainBank.java
+++ assessment2/MainBank.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MainBank {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		AccountDetails bank = new AccountDetails(1234567, 10000, "abi");
+		AccountDetails bank = new AccountDetails(1234567, 10000, "Stephen");
 		System.out.println("Enter your pin number: ");
 		long UserPin = sc.nextLong();
 		sc.nextLine();
